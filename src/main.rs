fc743a1c845d8c1ad8c253fe8faabe0fe4f1b79f
Rafael González <astrorafael@gmail.com>
2023-08-31 13:29:59 +0200

    added logging to log and console

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
+use std::path::PathBuf;
+use tracing::{Level};
+
 
 // Include this module as part of the binary, not the library
 // as this contains the actual implementation of the logging facility
 mod logging;
 
 fn main() {
-    logging::init(tracing::Level::DEBUG);
+    //logging::init(tracing::Level::DEBUG);
+    let mut _guards = logging::init(Level::INFO, true, Some(PathBuf::from("zptess.log")));
     zptess::database::init("zptess.db").unwrap();
 }
